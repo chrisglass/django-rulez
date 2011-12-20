48005240fa7e9b465a49906b9532e328f2ca8798
Alvin Mites <alvin@mitesdesign.com>
2011-12-19 22:58:25 -0700

    Added templatetag to allow accessing object permissions at a user level

---
+++ rulez/templatetags/rulez_perms.py
@@ -0,0 +1,52 @@
+from django import template
+from django.contrib.auth.models import AnonymousUser
+
+register = template.Library()
+
+class RulezPermsNode(template.Node):
+    def __init__(self, codename, objname, varname):
+        self.codename = codename
+        self.objname = objname
+        self.varname = varname
+
+    def render(self, context):
+        user_obj = template.resolve_variable('user', context)
+        obj = template.resolve_variable(self.objname, context)
+        if not user_obj.is_authenticated:
+            user_obj = AnonymousUser()
+        context[self.varname] = user_obj.has_perm(self.codename, obj)
+        return ''
+
+def rulez_perms(parser, token):
+    '''
+    Template tag to check for permission against an object.
+    Built out of a need to use permissions with anonymous users at an
+    object level.
+
+    Usage:
+        {% load rulez_perms %}
+
+        {% for VARNAME in QUERYRESULT %}
+            {% rulez_perms CODENAME VARNAME as BOOLEANVARNAME %}
+            {% if BOOLEANVARNAME %}
+                I DO
+            {% else %}
+                I DON'T
+            {% endif %}
+            have permission for {{ VARNAME }}.{{ CODENAME }}!!
+        {% endfor %}
+    '''
+    try:
+        bits = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            'tag requires exactly three arguments')
+    if len(bits) != 5:
+        raise template.TemplateSyntaxError(
+            'tag requires exactly three arguments')
+    if bits[3] != 'as':
+        raise template.TemplateSyntaxError(
+            "third argument to tag must be 'as'")
+    return RulezPermsNode(bits[1], bits[2], bits[3])
+
+rulez_perms = register.tag(rulez_perms)
