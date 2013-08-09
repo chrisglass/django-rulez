f88bb200473a023f2ce7eac28f21f10202ce3385
Chris Glass <tribaal@gmail.com>
2013-08-09 18:09:46 +0200

    Bumped version to 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = '1.0.0'
+version = '1.0.1'
 
 setup(
     name='django-rulez',
